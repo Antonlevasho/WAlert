903b43cdfbc45dcd3fe39b57192b2027c67db933
Антон Левашов <146174960+Antonlevasho@users.noreply.github.com>
2024-05-31 11:26:06 +0300

    убрал ошибки, создал кнопку, но она не появилась

--- AlertController/Classes/Class WAlertOptionButtons + protocol.swift
+++ AlertController/Classes/Class WAlertOptionButtons + protocol.swift
@@ -15,7 +15,8 @@ import UIKit
 
 class WAlertOptionButton: UIView {
 
-    var title: String
+    var info:  WAlertOptionButtonInfo?
+    
     
     var isHighlighted: Bool {
         didSet {
@@ -28,8 +29,8 @@ class WAlertOptionButton: UIView {
         }
     }
     
-    public init(title: String ) {
-        self.title = title
+    public init(info:  WAlertOptionButtonInfo?) {
+        self.info = info
         self.isHighlighted = false
         
         super.init(frame: .zero)
@@ -67,75 +68,3 @@ extension WAlertOptionButton {
 }
 
     
-//    weak var delegates: WAlertOptionButtonDelegate?
-
-    //MARK: - show bottons
-    
-//    func showButton(view: UIView) {
-//        self.center = view.center
-//        view.addSubview(self)
-//        
-//        
-//    }
-
-
-//    var isHighlighted: Bool {
-//        didSet {
-//            UIView.animate(withDuration: 0.5,
-//                            delay: .zero,
-//                            options: [.curveEaseOut, .allowUserInteraction],
-//                           animations: {
-//                // тут положить дополнительные анимации при нажатии, например поменять цвет
-//            }, completion: nil)
-//        }
-//    }
-//}
-
-//extension WAlertOptionButtons {
-//
-////    weak var delegates: WAlertOptionButtonDelegate?
-//
-//    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        animateToActiveState()
-//    }
-//
-//    public override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        handleTouch(touches, touchEnded: false)
-//    }
-//
-//    public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        handleTouch(touches, touchEnded: true)
-//    }
-//
-//    public override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        animateToDefaultState()
-//    }
-//
-//    func handleTouch(_ touches: Set<UITouch>, touchEnded: Bool) {
-//        if let touchLocationInSuperview: CGPoint = touches.first?.location(in: self.superview) {
-//            let touchLocation: CGPoint = self.convert(touchLocationInSuperview, to: self)
-//            if frame.contains(touchLocation) {
-//                if touchEnded {
-//                    animateToDefaultState()
-//                    delegates?.didTap(from: self)
-//                } else {
-//                    animateToActiveState()
-//                }
-//            } else {
-//                animateToDefaultState()
-//            }
-//        } else {
-//            animateToDefaultState()
-//        }
-//    }
-//
-//    func animateToActiveState() {
-//        isHighlighted = true
-//    }
-//
-//    func animateToDefaultState() {
-//        isHighlighted = false
-//    }
-//
-//
-//}
--- AlertController/ViewController.swift
+++ AlertController/ViewController.swift
@@ -7,132 +7,21 @@
 
 import UIKit
 
-//protocol WAlertOptionButtonDelegate: AnyObject {
-//    func didTap(from: UIView)
-//}
-
-//MARK: - Button UIView second
-//class WAlertOptionButtons: WAlertOptionButtonDelegate {
-//    func didTap(from: UIView) {
-//    }
-//    
-//    init() {
-//        
-//        self.isHighlighted = false
-//        super.init(frame: frame)
-//        
-//    }
-//    
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//    
-//    weak var delegates: WAlertOptionButtonDelegate?
-//    
-//    func setupButton() {
-//        
-//        guard let superview = self.superview else { return }
-//        self.translatesAutoresizingMaskIntoConstraints = false
-//        
-//        self.layer.cornerRadius = 20
-//        self.backgroundColor = .systemGray
-//        self.centerXAnchor.constraint(equalTo: superview.centerXAnchor, constant: 60).isActive = true
-//        self.centerYAnchor.constraint(equalTo: superview.centerYAnchor, constant: 40).isActive = true
-//        self.widthAnchor.constraint(equalToConstant: 100).isActive = true
-//        self.heightAnchor.constraint(equalToConstant: 50).isActive = true
-//    }
-//
-//    //MARK: - show bottons
-//    func showButtons(view: UIView) {
-//        self.center = view.center
-//        view.addSubview(self)
-//        setupButton()
-//    }
-//
-//
-//    var isHighlighted: Bool {
-//        didSet {
-//            UIView.animate(withDuration: 0.5,
-//                            delay: .zero,
-//                            options: [.curveEaseOut, .allowUserInteraction],
-//                           animations: {
-//                // тут положить дополнительные анимации при нажатии, например поменять цвет
-//            }, completion: nil)
-//        }
-//    }
-//}
-
-//MARK: - Button UIView one
-//class WAlertOptionButton: UIView {
-//    init(frame: CGRect, title: String) {
-//
-////        self.isHighlighted = false
-//        super.init(frame: frame)
-//
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//
-//    weak var delegate: WAlertOptionButtonDelegate?
-//
-//    func setupButton() {
-//        
-//        guard let superview = self.superview else { return }
-//        self.translatesAutoresizingMaskIntoConstraints = false
-//
-//        self.layer.cornerRadius = 20
-//        self.backgroundColor = .systemGray
-//        self.centerXAnchor.constraint(equalTo: superview.centerXAnchor, constant: -60).isActive = true
-//        self.centerYAnchor.constraint(equalTo: superview.centerYAnchor, constant: 40).isActive = true
-//        self.widthAnchor.constraint(equalToConstant: 100).isActive = true
-//        self.heightAnchor.constraint(equalToConstant: 50).isActive = true
-//    
-//  //  MARK: - show botton
-//    func showButton(view: UIView) {
-//        self.center = view.center
-//        view.addSubview(self)
-////        setupButton()
-//    }
-//}
-//    
-    //MARK: - metods button
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-    
-
-//    var isHighlighted: Bool {
-//        didSet {
-//            UIView.animate(withDuration: 0.5,
-//                            delay: .zero,
-//                            options: [.curveEaseOut, .allowUserInteraction],
-//                           animations: {
-//                // тут положить дополнительные анимации при нажатии, например поменять цвет
-//            }, completion: nil)
-//        }
-//    }
-    
-//}
-
 //MARK: - WAlert View
 class WAlert: UIView {
     
-    var title: String
-    
-    
+    var title: String?
     //MARK: - настройка вью(квадрата)
     override init(frame: CGRect) {
         
-        super.init(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        super.init(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
         self.title = "asdasd"
         self.backgroundColor = .lightGray
         self.layer.cornerRadius = 20
         
    
-        let info = WAlertOptionButtonInfo(title: "Hello")
-        let button = WAlertOptionButton(title: )
+        let info = WAlertOptionButtonInfo(title: "Andnn")
+        var button = WAlertOptionButton(info: info)
         
       
 
@@ -157,6 +46,7 @@ class WViewController: UIViewController {
         let alertController = WAlert(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
         alertController.showAlert(onView: self.view)
         
+      
         
         
 
