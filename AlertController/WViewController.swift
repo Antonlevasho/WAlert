b21d06781c7e23c670c00011abe4b5a4c1ccd5a0
Eduard <51046544+pressanykeyplease@users.noreply.github.com>
2024-06-06 14:37:29 +0300

    Update WViewController

--- AlertController/WViewController.swift
+++ AlertController/WViewController.swift
@@ -1,10 +1,17 @@
 import UIKit
     
-//MARK: - ViewController
+//MARK: - WViewController
 class WViewController: UIViewController {
     // MARK: - View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        initialize()
+    }
+}
+
+// MARK: - Private methods
+private extension WViewController {
+    func initialize() {
         view.backgroundColor = .white
         let alertController = WAlert(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
         alertController.showAlert(onView: self.view)
