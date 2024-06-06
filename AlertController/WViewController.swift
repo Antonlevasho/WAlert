d0c5855776069799de2fc3cfa805fe1ffa93e087
Eduard <51046544+pressanykeyplease@users.noreply.github.com>
2024-06-06 14:42:09 +0300

    Add alert button

--- AlertController/WViewController.swift
+++ AlertController/WViewController.swift
@@ -7,12 +7,29 @@ class WViewController: UIViewController {
         super.viewDidLoad()
         initialize()
     }
+
+    // MARK: - Private properties
+    private lazy var alertButton: UIButton = {
+        let view = UIButton(type: .system)
+        view.setTitle("Show alert", for: .normal)
+        view.addTarget(self, action: #selector(didTapAlertButton), for: .touchUpInside)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
 }
 
 // MARK: - Private methods
 private extension WViewController {
     func initialize() {
         view.backgroundColor = .white
+        view.addSubview(alertButton)
+        NSLayoutConstraint.activate([
+            alertButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: .zero),
+            alertButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: .zero),
+        ])
+    }
+
+    @objc func didTapAlertButton() {
         let alertController = WAlert(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
         alertController.showAlert(onView: self.view)
     }
